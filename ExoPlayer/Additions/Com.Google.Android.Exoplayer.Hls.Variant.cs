ea5654b4b677f020d65a05b6d18dbc96b3bb75c9
Nezz <bloodsource@vipmail.hu>
2015-01-21 05:12:43 +0100

    Preparation for HLS branch

---
+++ ExoPlayer/Additions/Com.Google.Android.Exoplayer.Hls.Variant.cs
@@ -0,0 +1,13 @@
+/*namespace Com.Google.Android.Exoplayer.Hls
+{
+    public partial class Variant
+    {
+        public partial class DecreasingBandwidthComparator
+        {
+            int Java.Util.IComparator.Compare(Java.Lang.Object obj1, Java.Lang.Object obj2)
+            {
+                return Compare((Com.Google.Android.Exoplayer.Hls.Variant)obj1, (Com.Google.Android.Exoplayer.Hls.Variant)obj2);
+            }
+        }
+    }
+}*/
